055f3b726e464157b57c43acacfbe5df1d20c206
lcl9288 <2450600633@qq.com>
2018-09-12 18:29:57 -0700

    custom

--- www/alipay.js
+++ www/alipay.js
@@ -1,5 +1,11 @@
 var exec = require('cordova/exec');
 
-exports.payment = function(payInfo, success, error) {
+
+exports.payment = function (payInfo, success, error) {
     exec(success, error, "alipay", "payment", [payInfo]);
 };
+exports.authV2 = function (payInfo, success, error) {
+    exec(success, error, "alipay", "auth", [payInfo]);
+};
+
+
