c06c0739d9a1b27af81bf82249e42d86da6fedc5
lcl9288 <2450600633@qq.com>
2018-10-26 14:25:54 +0800

    fixed bug

--- src/ios/alipay.m
+++ src/ios/alipay.m
@@ -43,7 +43,7 @@
 {
     callbackId = command.callbackId;
     NSString* authString = [command.arguments objectAtIndex:0];
-    NSString* appScheme = [NSString stringWithFormat:@"authali%@", app_id];
+    NSString* appScheme = [NSString stringWithFormat:@"ali%@", app_id];
     [[AlipaySDK defaultService] auth_V2WithInfo: authString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
                                            NSLog(@"%@",resultDic);
         CDVPluginResult* pluginResult;
@@ -60,38 +60,80 @@
 
 
 
-- (void)handleOpenURL:(NSNotification *)notification {
-    NSURL* url = [notification object];
-    
-    if ([url isKindOfClass:[NSURL class]] && [url.scheme isEqualToString:[NSString stringWithFormat:@"ali%@", app_id]])
-    {
-        
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
+{
+    if ([url.host isEqualToString:@"safepay"]) {
+        // 支付跳转支付宝钱包进行支付，处理支付结果
         [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
-            
-            
+            NSLog(@"result = %@",resultDic);
             CDVPluginResult* pluginResult;
             
-            if ([[resultDic objectForKey:@"resultStatus"]  isEqual: @"9000"]) {
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDic];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
-            } else {
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:resultDic];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
-            }
-        }];
-    }
-    if ([url isKindOfClass:[NSURL class]] && [url.scheme isEqualToString:[NSString stringWithFormat:@"authali%@", app_id]])
-    {
-        [[AlipaySDK defaultService] processAuth_V2Result:url standbyCallback:^(NSDictionary *resultDic) {
-            NSLog(@"dddd%@",resultDic);
-            CDVPluginResult* pluginResult;
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDic];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+                        if ([[resultDic objectForKey:@"resultStatus"]  isEqual: @"9000"]) {
+                            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDic];
+                            [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+                        } else {
+                            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:resultDic];
+                            [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+                        }
+            
         }];
         
+        // 授权跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processAuth_V2Result:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"result = %@",resultDic);
+            // 解析 auth code
+            NSString *result = resultDic[@"result"];
+                 NSLog(@"result = %@",resultDic[@"result"]);
+            NSString *authCode = nil;
+            
+//            if (result.length>0) {
+//                NSArray *resultArr = [result componentsSeparatedByString:@"&"];
+//                for (NSString *subResult in resultArr) {
+//                    if (subResult.length > 10 && [subResult hasPrefix:@"auth_code="]) {
+//                        authCode = [subResult substringFromIndex:10];
+//                        break;
+//                    }
+//                }
+//            }
+//            NSLog(@"授权结果 authCode = %@", authCode?:@"");
+        }];
     }
-    
+    return YES;
 }
+
+
+//- (void)handleOpenURL:(NSNotification *)notification {
+//    NSURL* url = [notification object];
+//
+//    if ([url isKindOfClass:[NSURL class]] && [url.scheme isEqualToString:[NSString stringWithFormat:@"ali%@", app_id]])
+//    {
+//
+//        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+//
+//
+//            CDVPluginResult* pluginResult;
+//
+//            if ([[resultDic objectForKey:@"resultStatus"]  isEqual: @"9000"]) {
+//                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDic];
+//                [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+//            } else {
+//                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:resultDic];
+//                [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+//            }
+//        }];
+//    }
+//    if ([url isKindOfClass:[NSURL class]] && [url.scheme isEqualToString:[NSString stringWithFormat:@"authali%@", app_id]])
+//    {
+//        [[AlipaySDK defaultService] processAuth_V2Result:url standbyCallback:^(NSDictionary *resultDic) {
+//            NSLog(@"dddd%@",resultDic);
+//            CDVPluginResult* pluginResult;
+//            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDic];
+//            [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+//        }];
+//
+//    }
+//
+//}
 
 
 
